300f951c759f443fd67e90c13342d5caf3fd5618
Roy <roy@codecraftr.nl>
2022-04-26 11:24:45 +0200

    return next session length

--- staydog/sessions.py
+++ staydog/sessions.py
@@ -1,10 +1,5 @@
+import math
+
+
 def next_session_length(passed_session_count):
-    if passed_session_count == 1:
-        return 2
-    if passed_session_count == 2:
-        return 4
-    if passed_session_count == 3:
-        return 8
-    if passed_session_count == 4:
-        return 16
-    return 1
+    return 2 ** passed_session_count
