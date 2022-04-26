9f617671e5349c7f6ec6938d7efb7a1fa9377b60
Roy <roy@codecraftr.nl>
2022-04-26 11:35:42 +0200

    precommit hooks

--- staydog/sessions.py
+++ staydog/sessions.py
@@ -1,5 +1,2 @@
-import math
-
-
 def next_session_length(passed_session_count):
-    return 2 ** passed_session_count
+    return 2**passed_session_count
--- tests/test_sessions.py
+++ tests/test_sessions.py
@@ -4,6 +4,9 @@ from staydog.sessions import next_session_length
 
 
 class TestSessions:
-    @pytest.mark.parametrize("passed_session_count,expected_minutes", [(0, 1), (1, 2), (2, 4), (3, 8), (4, 16)])
+    @pytest.mark.parametrize(
+        "passed_session_count,expected_minutes",
+        [(0, 1), (1, 2), (2, 4), (3, 8), (4, 16)],
+    )
     def test_next_session_length(self, passed_session_count, expected_minutes):
         assert next_session_length(passed_session_count) == expected_minutes
