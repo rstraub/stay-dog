9ddfda3e1082f57a9f5e1db7a5cbf0f89dcb4a6e
Roy <roy@codecraftr.nl>
2022-04-26 15:37:40 +0200

    super simplistic implementation to update session counter

--- staydog/app.py
+++ staydog/app.py
@@ -1,19 +1,21 @@
-from session_repository import get_session_count
+from session_repository import get_session_count, update_session_count
 from sessions import next_session_duration
 
 
 def main():
     successful_sessions = get_session_count()
-    print("Completed sessions so far: " + successful_sessions)
+    print(f"Completed sessions so far: {successful_sessions}")
 
-    next_session = next_session_duration(int(successful_sessions))
-    print(f"Did the dog stay alone successfully for {next_session} minutes? (y/n)")
+    session_duration = next_session_duration(int(successful_sessions))
+    print(f"Did the dog stay alone successfully for {session_duration} minutes? (y/n)")
 
     y_or_n = input()
     if y_or_n == "n":
         print("Too bad, next session will be shorter")
+        update_session_count(successful_sessions - 1)
     elif y_or_n == "y":
         print("Great! Next session will be longer")
+        update_session_count(successful_sessions + 1)
     else:
         print("Please enter 'y' or 'n'")
 
--- staydog/session_repository.py
+++ staydog/session_repository.py
@@ -4,7 +4,7 @@ __SESSIONS_FILE = ".staydog-sessions"
 def get_session_count():
     file = open(__SESSIONS_FILE, "r")
     stored_session_count = file.readlines()[0].replace("\n", "")
-    return stored_session_count
+    return int(stored_session_count)
 
 
 # Assumes there is one line in the file containing the number of completed sessions
