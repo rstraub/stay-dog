d4aeeec093007ac240787d3c1b69911e2db85e3a
Roy <roy@codecraftr.nl>
2022-04-26 15:32:06 +0200

    removed the booleans

--- staydog/app.py
+++ staydog/app.py
@@ -7,7 +7,15 @@ def main():
     print("Completed sessions so far: " + successful_sessions)
 
     next_session = next_session_duration(int(successful_sessions))
-    print("Next session: " + str(next_session) + " minute(s)")
+    print(f"Did the dog stay alone successfully for {next_session} minutes? (y/n)")
+
+    y_or_n = input()
+    if y_or_n == "n":
+        print("Too bad, next session will be shorter")
+    elif y_or_n == "y":
+        print("Great! Next session will be longer")
+    else:
+        print("Please enter 'y' or 'n'")
 
 
 if __name__ == "__main__":
