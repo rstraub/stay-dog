4d2a68c8a976b03696ceeaf50d5aa50337a1a092
Roy <roy@codecraftr.nl>
2022-04-26 15:42:16 +0200

    show the next number of minutes for a session

--- staydog/app.py
+++ staydog/app.py
@@ -4,18 +4,24 @@ from sessions import next_session_duration
 
 def main():
     successful_sessions = get_session_count()
-    print(f"Completed sessions so far: {successful_sessions}")
+    print(f"Successful sessions so far: {successful_sessions}")
 
     session_duration = next_session_duration(int(successful_sessions))
-    print(f"Did the dog stay alone successfully for {session_duration} minutes? (y/n)")
+    print(
+        f"Did the dog stay alone successfully for {session_duration} minute(s)? (y/n)"
+    )
 
     y_or_n = input()
     if y_or_n == "n":
-        print("Too bad, next session will be shorter")
-        update_session_count(successful_sessions - 1)
+        successful_sessions = successful_sessions - 1
+        update_session_count(successful_sessions)
+        next_duration = next_session_duration(successful_sessions)
+        print(f"Too bad, next session will be {next_duration} minute(s)")
     elif y_or_n == "y":
-        print("Great! Next session will be longer")
-        update_session_count(successful_sessions + 1)
+        successful_sessions = successful_sessions + 1
+        update_session_count(successful_sessions)
+        next_duration = next_session_duration(successful_sessions)
+        print(f"Great! Next session will be {next_duration} minute(s)")
     else:
         print("Please enter 'y' or 'n'")
 
