e406c0001dd4f7c9d8748bad196735ae789771fc
Roy <roy@codecraftr.nl>
2022-04-26 15:06:31 +0200

    Ugly way, but stores the completed sessions in a file

--- staydog/session_repository.py
+++ staydog/session_repository.py
@@ -2,9 +2,20 @@ __COMPLETED_SESSIONS = 0
 
 
 def get_session_count():
+    print(__get_stored_session_count())
     return __COMPLETED_SESSIONS
 
 
+# Assumes there is one line in the file containing the number of completed sessions
+def __get_stored_session_count():
+    file = open(".staydog-sessions", "r")
+    stored_session_count = file.readlines()[0].replace("\n", "")
+    return stored_session_count
+
+
 def update_session_count(completed_count):
+    file = open(".staydog-sessions", "w")
+    file.write(str(completed_count))
+    file.close()
     global __COMPLETED_SESSIONS
     __COMPLETED_SESSIONS = completed_count
