68efe1a6ca1a05954e367a7632bbc3f6a4703b61
Roy <roy@codecraftr.nl>
2022-04-26 15:09:14 +0200

    get rid of the in memory implementation

--- staydog/app.py
+++ staydog/app.py
@@ -4,9 +4,9 @@ from sessions import next_session_duration
 
 def main():
     successful_sessions = get_session_count()
-    print("Completed sessions so far: " + str(successful_sessions))
+    print("Completed sessions so far: " + successful_sessions)
 
-    next_session = next_session_duration(successful_sessions)
+    next_session = next_session_duration(int(successful_sessions))
     print("Next session: " + str(next_session) + " minute(s)")
 
 
--- staydog/session_repository.py
+++ staydog/session_repository.py
@@ -1,21 +1,16 @@
-__COMPLETED_SESSIONS = 0
+__SESSIONS_FILE = ".staydog-sessions"
 
 
 def get_session_count():
-    print(__get_stored_session_count())
-    return __COMPLETED_SESSIONS
-
-
-# Assumes there is one line in the file containing the number of completed sessions
-def __get_stored_session_count():
-    file = open(".staydog-sessions", "r")
+    file = open(__SESSIONS_FILE, "r")
     stored_session_count = file.readlines()[0].replace("\n", "")
     return stored_session_count
 
 
+# Assumes there is one line in the file containing the number of completed sessions
+
+
 def update_session_count(completed_count):
-    file = open(".staydog-sessions", "w")
+    file = open(__SESSIONS_FILE, "w")
     file.write(str(completed_count))
     file.close()
-    global __COMPLETED_SESSIONS
-    __COMPLETED_SESSIONS = completed_count
