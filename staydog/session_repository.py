871c17c5fc65d902942a26c18dcb5ec8d337d972
Roy <roy@codecraftr.nl>
2022-04-26 16:06:12 +0200

    little bit of error handling on files

--- staydog/session_repository.py
+++ staydog/session_repository.py
@@ -2,15 +2,19 @@ __SESSIONS_FILE = ".staydog-sessions"
 
 
 def get_session_count():
-    file = open(__SESSIONS_FILE, "r")
-    stored_session_count = file.readlines()[0].replace("\n", "")
-    return int(stored_session_count)
+    try:
+        contents = open(__SESSIONS_FILE).readline()
+        stored_session_count = int(contents.replace("\n", ""))
+        return stored_session_count
+    except IOError:
+        print("No previous sessions found")
+        return 0
 
 
 # Assumes there is one line in the file containing the number of completed sessions
 
 
 def update_session_count(completed_count):
-    file = open(__SESSIONS_FILE, "w")
+    file = open(__SESSIONS_FILE, "w+")
     file.write(str(completed_count))
     file.close()
