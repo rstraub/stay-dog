298f61ea9773bfc9f85835ef1b95316df98452c7
Roy <roy@codecraftr.nl>
2022-04-26 13:40:09 +0200

    using array instead of exponent

--- staydog/sessions.py
+++ staydog/sessions.py
@@ -1,2 +1,6 @@
 def next_session_length(passed_session_count):
-    return 2**passed_session_count
+    return session_lengths()[passed_session_count]
+
+
+def session_lengths():
+    return [1, 2, 4, 8, 16, 24, 32]
--- tests/test_sessions.py
+++ tests/test_sessions.py
@@ -8,7 +8,16 @@ class TestSessions:
         "passed_session_count,expected_minutes",
         [(0, 1), (1, 2), (2, 4), (3, 8), (4, 16)],
     )
-    def test_next_session_length_should_double_given_16_minutes_or_less(
+    def test_session_length_should_double_given_4_or_less_passed(
+        self, passed_session_count, expected_minutes
+    ):
+        assert next_session_length(passed_session_count) == expected_minutes
+
+    @pytest.mark.parametrize(
+        "passed_session_count,expected_minutes",
+        [(5, 24), (6, 32)],
+    )
+    def test_session_length_should_increment_given_5_or_more_passed(
         self, passed_session_count, expected_minutes
     ):
         assert next_session_length(passed_session_count) == expected_minutes
