d7b074753e586d645698bd8db1eeb0e14642b7f9
Roy <roy@codecraftr.nl>
2022-04-26 11:08:54 +0200

    Started work on determining next session length

--- staydog/sessions.py
+++ staydog/sessions.py
@@ -1,2 +1,6 @@
-def next_session_length():
+def next_session_length(passed_session_count):
+    if passed_session_count == 1:
+        return 2
+    if passed_session_count == 2:
+        return 4
     return 1
--- tests/test_sessions.py
+++ tests/test_sessions.py
@@ -3,5 +3,13 @@ from staydog.sessions import next_session_length
 
 class TestSessions:
     def test_first_session_should_be_one_minute(self):
-        result = next_session_length()
+        result = next_session_length(0)
         assert result == 1
+
+    def test_second_session_should_be_two_minutes(self):
+        result = next_session_length(1)
+        assert result == 2
+
+    def test_third_session_should_be_four_minutes(self):
+        result = next_session_length(2)
+        assert result == 4
