f935b2b2f05fef1be640282ac700e45aff883a97
Roy <roy@codecraftr.nl>
2022-04-26 12:11:41 +0200

    test name

--- tests/test_sessions.py
+++ tests/test_sessions.py
@@ -8,5 +8,7 @@ class TestSessions:
         "passed_session_count,expected_minutes",
         [(0, 1), (1, 2), (2, 4), (3, 8), (4, 16)],
     )
-    def test_next_session_length(self, passed_session_count, expected_minutes):
+    def test_next_session_length_should_double_given_16_minutes_or_less(
+        self, passed_session_count, expected_minutes
+    ):
         assert next_session_length(passed_session_count) == expected_minutes
